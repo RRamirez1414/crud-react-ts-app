94b613e50f95742b014120a6efe84b8c12e9ef01
Ramon <ramirez.ramon1414@gmail.com>
2021-05-17 22:52:10 -0600

    PR changes, cleaned up package.json and styled components

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -13,6 +13,9 @@ export default CardGrid
 
 const Grid = tw.div`grid grid-cols-1 px-40 justify-items-center
   lg:grid-cols-2
+<<<<<<< HEAD
   xl:grid-cols-3
+=======
+>>>>>>> PR changes, cleaned up package.json and styled components
   2xl:grid-cols-4
 `
