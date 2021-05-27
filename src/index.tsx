26b61ef34545f95f9bdd56ff5c2b6d629e8afa6c
Ramon <ramirez.ramon1414@gmail.com>
2021-05-27 16:07:20 -0600

    Initialized logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import GlobalStyles from 'styles/GlobalStyles'
+import LogRocket from 'logrocket'
+
+LogRocket.init('pnpjrs/pocket-collection')
 
 //import mocks/browser.ts file conditionally
 if (window.Cypress) {
