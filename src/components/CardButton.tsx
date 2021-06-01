6e1289acf5909e831cc0e09d23e91b70a981b745
Ramon <ramirez.ramon1414@gmail.com>
2021-06-01 10:38:11 -0600

    removed some warnings

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useCollection } from 'hooks'
 import { useTranslation } from 'react-i18next'
 import { motion } from 'framer-motion'
